23657341ef8a9047cdf2a000e1cddba65ee3d6f6
Yoana <yoanabarbie@icloud.com>
2024-09-10 19:07:57 +0200

    feat: basic config indicated

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,21 @@
 import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors())
+app.use(express.json())
+
+//CONFIG
+const PORT = process.env.PORT || 4000;
+
+AppDataSource.initialize()
+    .then(() => {
+        console.log('Database connected');
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`)
+        })
+    })
+    .catch(error => {
+        console.log(error)
+    })
