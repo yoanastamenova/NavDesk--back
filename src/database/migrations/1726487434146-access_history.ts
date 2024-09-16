02848962c7bec8e8a11a961a71932513242293fe
Yoana <yoanabarbie@icloud.com>
2024-09-16 14:51:07 +0200

    feat: access_history table completed

--- src/database/migrations/1726487434146-access_history.ts
+++ src/database/migrations/1726487434146-access_history.ts
@@ -1,11 +1,57 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class AccessHistory1726487434146 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "access_history",
+                columns: [
+                    {
+                        name: "id",
+                        type: "int",
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: "increment"
+                    },
+                    {
+                        name: "person_id",
+                        type: "int",
+                        isNullable: false
+                    },
+                    {
+                        name: "room_id",
+                        type: "int",
+                        isNullable: false
+                    },
+                    {
+                        name: "entry_datetime",
+                        type: "datetime"
+                    },
+                    {
+                        name: "exit_datetime",
+                        type: "datetime"
+                    }
+                ],
+                foreignKeys: [
+                    {
+                        columnNames: ["person_id"],
+                        referencedTableName: "person",
+                        referencedColumnNames: ["id"],
+                        onDelete: "CASCADE"
+                    },
+                    {
+                        columnNames: ["room_id"],
+                        referencedTableName: "room",
+                        referencedColumnNames: ["id"],
+                        onDelete: "CASCADE"
+                    }
+                ],
+            }),
+            true
+        );
+    }        
+        public async down(queryRunner: QueryRunner): Promise<void> {
+            await queryRunner.dropTable('access_history')
+        }
     }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
-}
