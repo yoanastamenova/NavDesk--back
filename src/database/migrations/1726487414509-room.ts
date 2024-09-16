685113f9abb03cb7ecbd9c926a0cd0a46c912ce7
Yoana <yoanabarbie@icloud.com>
2024-09-16 14:17:23 +0200

    feat: room migration completed

--- src/database/migrations/1726487414509-room.ts
+++ src/database/migrations/1726487414509-room.ts
@@ -1,11 +1,43 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class Room1726487414509 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "room",
+                columns: [
+                    {
+                        name: "id",
+                        type: "int",
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: "increment"
+                    },
+                    {
+                        name: "room_name",
+                        type: "varchar",
+                        length: "100",
+                        isNullable: false
+                    },
+                    {
+                        name: "capacity",
+                        type: "int",
+                        isNullable: false
+                    },
+                    {
+                        name: "room_type",
+                        type: "varchar",
+                        length: "100"
+                    }
+                ]
+            }),
+            true
+        )
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('room')
     }
 
 }
