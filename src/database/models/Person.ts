b2a5de53713ab8953d3031bda2f15b9fefb8789c
Yoana <yoanabarbie@icloud.com>
2024-09-16 16:03:58 +0200

    feat: Person model added

--- src/database/models/Person.ts
+++ src/database/models/Person.ts
@@ -1,6 +1,25 @@
-import { Entity } from "typeorm"
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm"
 
 @Entity()
 export class Person {
+    @PrimaryGeneratedColumn()
+    id!: number
 
+    @Column({ name: 'first_name'})
+    first_name!: string
+
+    @Column({ name: 'last_name'})
+    last_name!: string
+
+    @Column({ name: 'startup'})
+    startup!: string
+
+    @Column({ name: 'email'})
+    email!: string
+
+    @Column({ name: 'dni'})
+    dni!: string
+
+    @Column({ name: 'phone'})
+    phone!: string
 }
