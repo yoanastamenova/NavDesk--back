d749bf7aa7c692f2716969720f6a14564994cf64
Yoana <yoanabarbie@icloud.com>
2024-09-16 16:08:28 +0200

    feat: added Room entity model

--- src/database/models/Room.ts
+++ src/database/models/Room.ts
@@ -1,6 +1,16 @@
-import { Entity } from "typeorm"
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class Room {
+    @PrimaryGeneratedColumn()
+    id!: number;
 
+    @Column({ name: 'room_name' })
+    room_name!: string;
+
+    @Column({ name: 'capacity' })
+    capacity!: number;
+
+    @Column({ name: 'room_type' })
+    room_type!: string;
 }
