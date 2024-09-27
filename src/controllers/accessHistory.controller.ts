63588406fd52434bde45aa81b3415925c885896f
Yoana <yoanabarbie@icloud.com>
2024-09-27 16:47:08 +0200

    feat: history controller + routes started

---
+++ src/controllers/accessHistory.controller.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from "express";
+import { Access_history } from "../database/models/Access_history";
+
+//GET HISTORY BY DATE
+export const getHistories = async (req: Request, res: Response) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+//GET HISTORY BY ROOM ID
+export const getRoomHistory = async (req: Request, res: Response) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
---
+++ src/routes/historyRouter.ts
@@ -0,0 +1,8 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get('/')
+router.get('/room/:id')
+
+export default router;
