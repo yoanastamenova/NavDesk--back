9651f0100ccc69e3c3809ce4189147f6d7c27e97
Yoana <yoanabarbie@icloud.com>
2024-09-28 11:49:44 +0200

    feat: report get routes completed

--- src/controllers/administration.controller.ts
+++ src/controllers/administration.controller.ts
@@ -1,8 +1,9 @@
 import { Request, Response } from "express";
 import { Access } from "../database/models/Access";
+import { Administration } from "../database/models/Administration";
 
 // OBTAIN A DAILY REPORT
-export const register = async (req: Request, res: Response) => {
+export const getDailyReport = async (req: Request, res: Response) => {
     try {
         const date = req.body.date;
 
@@ -45,3 +46,51 @@ export const register = async (req: Request, res: Response) => {
     }
 }
 
+//OBTAIN DATETIME PERIOD REPORT
+export const getDateReport = async (req: Request, res: Response) => {
+    try {
+            const date = req.body.date;
+    
+            const period = await Administration.find(
+                {
+                    select: {
+                        report_date: true,
+                        total_absences: true,
+                        total_accesses: true,
+                        frequent_users: true,
+                        infrequent_users: true
+                    },
+                    where: {
+                        report_date: date
+                    }
+                }
+            )
+    
+            if(!period) {
+                return res.status(404).json(
+                    {
+                        success: false,
+                        message: "Cannot obtain report for this date!"
+                    }
+                )
+            }
+    
+            res.status(200).json(
+                {
+                    success: true,
+                    message: "The Report for this date was retrived successfully!",
+                    data: period
+                }
+            )
+        }
+     catch (error) {
+        res.status(500).json(
+            {
+                success: false,
+
+            }
+        )
+     }
+}
+
+// REPORT FOR SPECIFIC ROOM
