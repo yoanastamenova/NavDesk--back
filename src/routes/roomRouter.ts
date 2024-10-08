213d8a859dfa08cdafa4c3d13588dab8af81ddd4
Yoana <yoanabarbie@icloud.com>
2024-10-08 19:59:25 +0200

    feat: removed auth from Room State

--- src/routes/roomRouter.ts
+++ src/routes/roomRouter.ts
@@ -14,7 +14,7 @@ const router = express.Router();
 
 router.get('/all',getAllRooms);
 router.get('/:id', auth, getRoomById);
-router.get('/:id/current-state', auth, getRoomCurrentStatus);
+router.get('/:id/current-state', getRoomCurrentStatus);
 router.post('/create', auth, isAdmin, createRoom); 
 router.put('/:id/update', auth, isAdmin, updateRoom);
 router.delete('/:id/delete', auth, isAdmin, deleteRoom); 
